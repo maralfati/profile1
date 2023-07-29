689636353735d9cb50a59e33ab8305205a85523f
maralfati <fatimahabeb855@gmail.com>
2023-07-29 11:00:21 -0700

    cv1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// let num=[1,4,8,88,657,76]
+// for(let i=0;i<num.length;i++){
+//     if(i==1){
+//         break
+//     }
+//     console.log(`${num[i]}`);
+// }
+let bob=[89,"k",66,"pp"]
+for(let ii=0;ii<bob.length;ii++){
+    if(ii==2){
+        continue
+    }
+    console.log(`${bob[ii]}`);
+}
